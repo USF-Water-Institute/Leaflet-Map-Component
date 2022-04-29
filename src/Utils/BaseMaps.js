ce9ffc47ca85a0a987ad88435cbf360fd8c488b6
Keith Bornhorst <bornhorst@usf.edu>
2022-04-29 09:49:24 -0400

    Added a GRAY basemap option

--- src/Utils/BaseMaps.js
+++ src/Utils/BaseMaps.js
@@ -6,6 +6,8 @@ import {
 export const getBaseMaps = () => {
     const imageryLabels = basemapLayer('ImageryLabels');
     const imagery = basemapLayer('ImageryFirefly');
+    const gray = basemapLayer('Gray');
+    const grayLabels = basemapLayer('GrayLabels');
     const streets = basemapLayer('Streets');
     const topo = basemapLayer('Topographic');
 
@@ -13,6 +15,7 @@ export const getBaseMaps = () => {
         Streets: streets,
         Aerials: imagery,
         'Aerial with labels': layerGroup([imagery, imageryLabels]),
+        'Gray': layerGroup([gray, grayLabels]),
         Topographic: topo
     };
 
